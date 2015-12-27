a5b8a3d73577293e263cac2afe8ab19e2176f305
naturalog <ohad@idni.org>
2015-12-27 19:28:08 +0200

    randomize the generator even more

--- gen.cpp
+++ gen.cpp
@@ -14,12 +14,13 @@ int main(int, char **argv) {
 	int sz = atoi(argv[1]);
 	mpreal::set_default_prec(2 * sz);
 	random(time(0));
+	srand(time(0));
 	mpreal M = pow(mpreal(2), sz);
 	cout.precision(sz*2);
 	mpreal x, s = 0;
 	for (int n = 0; n < sz-1; ++n) {
 		cerr << (x = ceil(M*mpfr::random())) << '\t';
-		s += pow(-1,n)*x;
+		s += pow(-1,rand())*x;
 		cout << x << endl;
 		cerr << s << endl;
 	}
